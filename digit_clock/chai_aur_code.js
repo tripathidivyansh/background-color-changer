447ec702d2a5458dd9ef40ea685c1e040947ebda
tripathidivyansh <tripatidivyansh144@gmail.com>
2025-04-27 13:37:56 +0530

    Digit_Colck

---
+++ digit_clock/chai_aur_code.js
@@ -0,0 +1,7 @@
+const clock = document.getElementById('clock');
+// console.log(date.toLocaleTimeString());
+setInterval(function(){
+    let date = new Date();
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
