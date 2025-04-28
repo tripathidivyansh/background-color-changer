4d44cf08727915bd2672f5fde115d5269cefb3e8
tripathidivyansh <tripatidivyansh144@gmail.com>
2025-04-28 12:23:20 +0530

    Random bg-changer

---
+++ unlimited_color/chaiaurcode.js
@@ -0,0 +1,28 @@
+// generate a random color
+const randomcolor = function(){
+    const hex = "0123456789ABCDEF";
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+let intervalid;
+
+const startChangingColor = function(){
+    if (!intervalid) { 
+        intervalid = setInterval(changebgcolor, 1000);
+    }
+    
+    function changebgcolor(){
+        document.body.style.backgroundColor = randomcolor();
+    }
+};
+
+const stopChangingColor = function(){
+    clearInterval(intervalid);
+    intervalid = null;
+};
+document.querySelector("#start").addEventListener('click', startChangingColor);
+document.querySelector("#stop").addEventListener('click', stopChangingColor);
